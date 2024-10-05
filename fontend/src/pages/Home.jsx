4e6e12412eccc12dd7c8b81b3571c82fc454d982
susuhaohaki <nguyenhai250203@gmail.com>
2024-10-05 23:15:36 +0700

    update Home client

--- fontend/src/pages/Home.jsx
+++ fontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
 import OurPolicy from "../components/OurPolicy";
-import NewsletterBox from "../components/newsletterBox";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Home = () => {
   return (
